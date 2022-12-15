cebb33f50e66654f3f18f58fe1c9f9183c46a859
usuario <mlungut2812@m.educaand.es>
2022-12-15 09:04:30 +0100

    Added day 2

---
+++ variables.py
@@ -0,0 +1,12 @@
+# Day 2: 30 Days of python programming
+first_name = "Helena"
+last_name = "Troncoso"
+full_name = "Helena Troncoso"
+country = "España"
+city = "Jerez"
+age = 16
+year = 2022
+is_married = False
+is_true = True
+is_light_on = True
+a, b = 4, 8
