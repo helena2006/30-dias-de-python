570cde3fed68296cb03ac2ef59b206d56bb9913b
helena2006 <117343715+helena2006@users.noreply.github.com>
2022-12-16 18:09:27 +0100

    He arreglado unos fallos y quedan más

--- dia_2/variables.py
+++ dia_2/variables.py
@@ -16,28 +16,37 @@ print(type(full_name))
 print(len(full_name))
 print(len(last_name))
 
-num_one = 1
-num_two = 2
+num_one = 5     #4
+num_two = 4
 
-total = num_one + num_two
+total = num_one + num_two   #4i
 print(total)
 
-diff = num_two - num_one
+diff = num_two - num_one      #4ii
 print(diff)
 
-multiply = num_one * num_two
-print(multiply)
+product = num_one * num_two     #4iii
+print(product)
 
-divide = num_two / num_one
-print(divide)
+exp = pow(num_one , num_two)      #4iv
+print(exp)
 
-r = 30
+floor_division = num_one // num_two     #4v
+print(floor_division)
+
+division = num_one / num_two      #4vi
+print(division)
+
+remainder = num_two % num_one     #4vii
+print(remainder)
+
+r = 30      #5
 pi = 3,14
-diameter = 30*2
-areaofcircle = pi*r
-circumferencecircle = pi*diameter
+diameter = 30 * 2
+area_of_circle = pi * r
+circum_of_circle = pi * diameter
 
-print(areaofcircle , circumferencecircle)
+print(area_of_circle , circum_of_circle)
 
 run 
 help("keywords")
