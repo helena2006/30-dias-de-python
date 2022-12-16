ad2ea06e38a778b08f2d793928d9eda0b8c4d68e
htroper985@m.educaand.es <htroper985@m.educaand.es>
2022-12-16 11:13:10 +0100

    Día 2 terminado

--- dia_2/variables.py
+++ dia_2/variables.py
@@ -10,3 +10,34 @@ is_married = False
 is_true = True
 is_light_on = True
 a, b = 4, 8
+print(country , city)
+
+print(type(full_name))
+print(len(full_name))
+print(len(last_name))
+
+num_one = 1
+num_two = 2
+
+total = num_one + num_two
+print(total)
+
+diff = num_two - num_one
+print(diff)
+
+multiply = num_one * num_two
+print(multiply)
+
+divide = num_two / num_one
+print(divide)
+
+r = 30
+pi = 3,14
+diameter = 30*2
+areaofcircle = pi*r
+circumferencecircle = pi*diameter
+
+print(areaofcircle , circumferencecircle)
+
+run 
+help("keywords")
