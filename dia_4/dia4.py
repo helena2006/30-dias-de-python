4f636c4ddd30eed1d32e10b9345e666c913cca59
htroper985@m.educaand.es <htroper985@m.educaand.es>
2023-01-27 11:07:57 +0100

    dia 4 en proceso

---
+++ dia_4/dia4.py
@@ -0,0 +1,27 @@
+thirty = "Thirty"       #1
+days = "days"
+of = "of"
+python = "Python"
+space = "  "
+full_string = thirty + space + days + space + of + space + python
+print(full_string)
+
+coding = "Coding"       #2
+f0r = "for"
+all = "all"
+full_string_2 = coding + space + f0r + space + all
+print(full_string_2)
+
+company = full_string_2     #3
+print(company)      #4
+print(len(company))        #5
+print(company.upper())      #6
+print(company.lower())      #7
+
+print(company.capitalize())     #8
+print(company.title())
+print(company.swapcase())
+
+first_slice = company[0:3]      #9
+print(first_slice)
+
