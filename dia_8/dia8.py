77c6fd46c0f76fccf779217fb46f220cb0475b82
htroper985@m.educaand.es <htroper985@m.educaand.es>
2023-02-17 10:46:30 +0100

    dia 8 terminado

--- dia_8/dia8.py
+++ dia_8/dia8.py
@@ -4,7 +4,17 @@ dog["color"] = "Blue"
 dog["breed"] = "Spanish"
 dog["legs"] = "2"
 dog["age"] = "16"
-studen_dictionary = {"first_name" : "Helena" , "last_name" : "Troncoso" , "gender" : "female" , "age" : "16" , "marital_status" : "single" , "skills" : ["creativity", "imagine"] , "country" : "Spain" , "City" : "Jerez" , "address" : "street"}       #3
+
+studen_dictionary = {"first_name" : "Helena" , "last_name" : "Troncoso" , "gender" : "female" , "age" : 16 , "marital_status" : "single" , "skills" : ["creativity", "imagine"] , "country" : "Spain" , "City" : "Jerez" , "address" : {"city" : "Jerez" , "CP" : "11405"}}       #3
 print(len(dog))     #4
 values_skills = studen_dictionary["skills"].values()      #5
 type(studen_dictionary["skills"])
+
+keys = dog.keys()       #7
+print(keys)
+values = dog.values()       #8
+print(values)
+print(dog.items())      #9
+
+dog.pop("first_name")       #10
+del dog     #11
