08856686606e531d0c4ef2e31e53bba7aba9c7a3
htroper985@m.educaand.es <htroper985@m.educaand.es>
2023-03-16 09:21:05 +0100

    dia 11

--- dia_11/dia11.py
+++ dia_11/dia11.py
@@ -1,1 +1,12 @@
 #Level 1 - 1
+def add_two_numbers(number1,number2):
+    number3=number2+number1
+    return number3
+
+print(add_two_numbers(2,3))
+
+#Level 1 - 2
+pi = 2.14
+r = int(input("Inserta un radio: "))
+area_of_circle = pi * r * r
+print(area_of_circle)
